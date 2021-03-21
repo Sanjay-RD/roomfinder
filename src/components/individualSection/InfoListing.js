2fdca1771078b495fac73288b9a3a3316d4cfb3f
Sanjay-RD <rishidevsanjay9@gmail.com>
2021-03-21 21:41:51 +0545

    Room Information Added to RoomScreen

---
+++ src/components/individualSection/InfoListing.js
@@ -0,0 +1,85 @@
+import React from "react";
+
+const InfoListing = ({ room }) => {
+  return (
+    <div
+      className="room-flex"
+      style={{ marginTop: "30px", color: "#47856c", gridGap: "2rem" }}
+    >
+      <div>
+        <div style={flexContainer}>
+          <h4>
+            <i className="fa fa-money" aria-hidden="true"></i> Asking Price:
+          </h4>
+          <h4>Rs.{room.price}</h4>
+        </div>
+        <div className="line-brake"></div>
+        <div style={flexContainer}>
+          <h4>
+            <i className="fa fa-bed" aria-hidden="true"></i> Bedrooms:
+          </h4>
+          <h4>{room.bedroom}</h4>
+        </div>
+        <div className="line-brake"></div>
+        <div style={flexContainer}>
+          <h4>
+            <i className="fa fa-bath" aria-hidden="true"></i> Bathrooms:
+          </h4>
+          <h4>{room.bathroom}</h4>
+        </div>
+        <div className="line-brake"></div>
+        <div style={flexContainer}>
+          <h4>
+            <i className="fa fa-car" aria-hidden="true"></i> Garage:
+          </h4>
+          <h4>{room.garage}</h4>
+        </div>
+        <div className="line-brake"></div>
+      </div>
+      {/* second list */}
+      <div>
+        <div style={flexContainer}>
+          <h4>
+            <i className="fas fa-map-marker-alt icon-color"></i> location
+          </h4>
+          <h4>
+            {room.address}, {room.city}
+          </h4>
+        </div>
+        <div className="line-brake"></div>
+        {room.sqft && (
+          <div>
+            <div style={flexContainer}>
+              <h4>
+                <i className="fa fa-th-large" aria-hidden="true"></i> Square
+                Feet:
+              </h4>
+              <h4>Rs.{room.sqft}</h4>
+            </div>
+            <div className="line-brake"></div>
+          </div>
+        )}
+        <div style={flexContainer}>
+          <h4>
+            <i class="fa fa-clock-o" aria-hidden="true"></i> Listing Date:
+          </h4>
+          <h4>{room.start}</h4>
+        </div>
+        <div className="line-brake"></div>
+        <div style={flexContainer}>
+          <h4>
+            <i className="fa fa-user" aria-hidden="true"></i> Owner:
+          </h4>
+          <h4>{room.owner}</h4>
+        </div>
+        <div className="line-brake"></div>
+      </div>
+    </div>
+  );
+};
+const flexContainer = {
+  display: "flex",
+  justifyContent: "space-between",
+};
+
+export default InfoListing;
--- src/components/individualSection/RoomScreen.js
+++ src/components/individualSection/RoomScreen.js
@@ -7,6 +7,7 @@ import BackgroundImage from "../lightBox2/BackgroundImage";
 import ImageListing from "../lightBox2/ImageListing";
 
 import profile from "./profile.jpg";
+import InfoListing from "./InfoListing";
 
 const RoomScreen = ({ match }) => {
   const [click, setClick] = useState(false);
@@ -24,6 +25,7 @@ const RoomScreen = ({ match }) => {
   };
 
   const room = rooms.find((room) => room.id === parseInt(match.params.id));
+  console.log(room);
   return (
     <div style={{ marginBottom: "20px" }}>
       <Link to="/">
@@ -41,6 +43,31 @@ const RoomScreen = ({ match }) => {
           >
             <BackgroundImage image={image} />
           </div>
+          {/* Room Info Component */}
+          <InfoListing room={room} />
+          <div style={scamsNotice}>
+            <h3
+              style={{
+                textAlign: "center",
+                marginBottom: "10px",
+                color: "red",
+              }}
+            >
+              <i className="fa fa-exclamation-triangle" aria-hidden="true"></i>{" "}
+              Be safe. Beware of fraud & scams.
+            </h3>
+            <p>
+              chaiyo.com is NOT involved in transaction of any goods/services
+              listed in the website. It is only platform to share information.
+              You are directly contacting the person who has posted the
+              advertisement and you agree not to hold chaiyo.com responsible for
+              their act in any circumstances. <br /> We strongly encourage you
+              to take necessary precaution. Avoid advance payment, check goods
+              before purchasing, instead of cash use mobile wallets (IME Pay,
+              Esewa, Khalti, Connect Ips, etc) or bank transfer for payments.
+              For more read Safety Tips
+            </p>
+          </div>
         </div>
         <div className="seller-details">
           <div className="seller-container">
@@ -52,6 +79,10 @@ const RoomScreen = ({ match }) => {
               <h4>
                 Name: <br />
                 <span className="text-color">{room.owner}</span>
+              </h4>
+              <h4>
+                Mobile Phone: <br />
+                <span className="text-color">{room.contact}</span>
               </h4>
               <h4>
                 Email: <br />
@@ -70,6 +101,13 @@ const RoomScreen = ({ match }) => {
       </div>
     </div>
   );
+};
+
+const scamsNotice = {
+  border: "1px solid #ccc6c6",
+  padding: "10px",
+  borderRadius: "2px",
+  marginTop: "10px",
 };
 
 export default RoomScreen;
